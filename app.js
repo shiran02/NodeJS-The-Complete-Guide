8542ca6b0b60fc5e16ab306ac5138a516ca780e0
Shiran Kumarasingha <shirankumarasingha97@gmail.com>
2024-07-08 19:42:38 +0530

    section 5 finish

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const http = require('http');
+
 const express = require('express');
+
+const path =require('path');
+
+
+
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 app.use((req,res,next)=>{
-    console.log('in the middleware here');
-    next();
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.use((req,res,next)=>{
-    console.log('in the another middleware here');
-});
 
 
 
-const server =  http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
